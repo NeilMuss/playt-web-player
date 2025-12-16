801c6c6531c6dde5828ac9bf9bab3829f825bff7
NeilMuss <37507669+NeilMuss@users.noreply.github.com>
2025-12-16 06:20:40 -0500

    Moved press down logic to logo.js

--- logo.js
+++ logo.js
@@ -2,20 +2,23 @@ const playIcon = document.getElementById("center-play");
 
 if (playIcon) {
   function pressDown() {
-    gsap.to(playIcon, {
+    gsap.to("#center-play", {
       scale: 0.92,
       duration: 0.08,
-      ease: "power2.out"
+      ease: "power2.out",
+      transformOrigin: "50% 50%"
     });
   }
 
   function release() {
-    gsap.to(playIcon, {
+    gsap.to("#center-play", {
       scale: 1,
       duration: 0.12,
-      ease: "power2.out"
+      ease: "power2.out",
+      transformOrigin: "50% 50%"
     });
   }
+
 
   // Desktop
   playIcon.addEventListener("mousedown", pressDown);
